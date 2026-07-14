23c4f005dc24307dc2636c2321b1ccc4d7cbad08
Critian <cristianortizcucunuba@gmail.com>
2026-07-14 03:01:36 -0500

    se agrego un codigo para adivinar un numero dentro de un rango que el mismo usuario puede elegir

--- ai-challenges/001/adivina_numero.py
+++ ai-challenges/001/adivina_numero.py
@@ -1,2 +1,26 @@
-def main():
-    print("funcion principal")
+import random
+
+
+def validador(texto):
+    while True:
+        try:
+            numMAX = int(input(texto))
+            break
+        except:
+            print("error")
+    return numMAX
+
+numMAX= validador("elige max: ")
+numU = None
+numElegido= 0
+numR = random.randint(numElegido, numMAX)
+
+
+while numU != numR:
+    numU = validador("adivina el numero: ")
+    if numU == numR:
+        print("YOU WINNNN")
+    elif numU < numR:
+        print("the number is greater")
+    elif numU > numR:
+        print("the number is less")
